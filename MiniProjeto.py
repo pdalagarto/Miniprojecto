26164685aa272ed43338162a93c38ef44a69c065
pdalagarto <45435748+pdalagarto@users.noreply.github.com>
2019-06-05 15:15:43 +0100

    comentario

--- MiniProjeto.py
+++ MiniProjeto.py
@@ -3,6 +3,9 @@ from PyQt5.QtWidgets import QApplication
 import sys
 from app.Main import Main
 
+if se esta merda funcionar:
+    print("boa J")
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ui = Main()
